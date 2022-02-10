068acb7e913a71d9f1e69572905528523d165140
ima906@outlook.com <ivkinama@mer.ci.nsu.ru>
2022-02-10 15:10:39 +0700

    добавлена масштабируемая карта

--- src/Ivkina/Views/IvkinaMapWpf/Window1.xaml.cs
+++ src/Ivkina/Views/IvkinaMapWpf/Window1.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using GMap.NET;
+using System.Net;
+using GMap.NET.MapProviders;
 
 namespace IvkinaMapWpf
 {
@@ -22,6 +25,24 @@ namespace IvkinaMapWpf
         public Window1()
         {
             InitializeComponent();
+        }
+
+        private void Map_load(object sender, EventArgs e) 
+        {
+            gmap.Bearing = 0;
+            gmap.CanDragMap = true;
+            gmap.DragButton=MouseButton.Left;
+
+            gmap.MaxZoom = 18;
+            gmap.MinZoom = 5;
+            gmap.MouseWheelZoomType = MouseWheelZoomType.MousePositionWithoutCenter;
+            gmap.ShowTileGridLines = false;
+            gmap.Zoom = 10;
+            gmap.ShowCenter = false;
+
+            gmap.MapProvider = GMapProviders.GoogleMap;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            gmap.Position = new PointLatLng(54.989347, 73.368221);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
